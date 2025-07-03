fe543388fb1efa871f0e6b3cff06c2916ff83b2b
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-07-03 19:04:19 +0600

    benefits and bemerchant section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from './Banner';
 import Services from './services/Services';
 import ClientLogosMarquee from './clientLogosMarquee/ClientLogosMarquee';
+import Benefits from './benefets/Benefits';
+import BeMerchant from './beMerchant/BeMerchant';
 
 const Home = () => {
     return (
@@ -9,6 +11,8 @@ const Home = () => {
             <Banner></Banner>
             <Services></Services>
             <ClientLogosMarquee></ClientLogosMarquee>
+            <Benefits></Benefits>
+            <BeMerchant></BeMerchant>
         </div>
     );
 };
