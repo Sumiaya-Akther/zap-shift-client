ba92b5eda7e6ff702529ff2ce5e7f2e9ee0cc241
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-07-05 12:39:25 +0600

    my parcels and day-3 done

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router";
 import { router } from './router/router.jsx';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 import Aos from 'aos';
 import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 Aos.init();
+// Create a client
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='font-urbanist max-w-7xl mx-auto'>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
     </div>
   </StrictMode>,
 )
